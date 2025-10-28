da6696976cfdf479ec98086ed7370971e946a08d
Andreas Deleuran <andreasdelu@gmail.com>
2025-10-28 10:32:49 +0100

    Add new minimal nvim config

---
+++ config/nvim/minimal.lua
@@ -0,0 +1,8 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 200
+vim.cmd 'filetype plugin indent on'
+vim.cmd 'syntax enable'
