eb0ff01f71c0a2660d25959e011df85f82f6556f
Andreas Deleuran <andreasdelu@gmail.com>
2026-04-17 14:45:26 +0200

    add diagnostics navigation keybinds

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,24 @@ local map = vim.keymap.set
 
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic quickfix list' })
+map('n', '<leader>dd', function()
+  vim.diagnostic.jump { count = 1 }
+end, { desc = 'Next diagnostic' })
+map('n', '<leader>dD', function()
+  vim.diagnostic.jump { count = -1 }
+end, { desc = 'Previous diagnostic' })
+map('n', '<leader>de', function()
+  vim.diagnostic.jump { count = 1, severity = vim.diagnostic.severity.ERROR }
+end, { desc = 'Next diagnostic error' })
+map('n', '<leader>dE', function()
+  vim.diagnostic.jump { count = -1, severity = vim.diagnostic.severity.ERROR }
+end, { desc = 'Previous diagnostic error' })
+map('n', '<leader>dw', function()
+  vim.diagnostic.jump { count = 1, severity = vim.diagnostic.severity.WARN }
+end, { desc = 'Next diagnostic warning' })
+map('n', '<leader>dW', function()
+  vim.diagnostic.jump { count = -1, severity = vim.diagnostic.severity.WARN }
+end, { desc = 'Previous diagnostic warning' })
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
