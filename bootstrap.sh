af05ba94d49034be62dbe0b82dec2856becaa19e
Andreas Deleuran <andreasdelu@gmail.com>
2026-05-29 17:13:07 +0200

    tpm stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,10 @@ run_step install_ohmyzsh.sh
 run_step authenticate_git.sh
 run_step setup_dotfiles.sh
 
+if [ ! -d "$HOME/.tmux/plugins/tpm/.git" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
+
 echo ""
 echo "--------------------------------"
 echo "Installing brew packages..."
